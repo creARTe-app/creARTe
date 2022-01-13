91c4aae937a5304226fe9117421c1cc2c3c1290d
Faizzann <71325258+Faizzann@users.noreply.github.com>
2022-01-13 18:12:55 +0400

    initial implementation complete

--- creARTe/crearte/client/src/components/Posts/Posts.js
+++ creARTe/crearte/client/src/components/Posts/Posts.js
@@ -1,20 +1,25 @@
-import React from 'react'
+import React from 'react';
+import { Grid, CircularProgress } from '@material-ui/core';
 import { useSelector } from 'react-redux';
-import Post from '../Posts/Post/Post'
-import useStyles from './styles.js'
 
-function Posts() {
-    const posts = useSelector((state) => state.posts);
-    const classes = useStyles();
+import Post from './Post/Post';
+import useStyles from './styles';
 
-    console.log(posts);
-    return (
-        <>
-        <h1>POSTS</h1>
-        <Post/>
-        <Post/>
-        </>
+const Posts = ({ setCurrentId }) => {
+  const posts = useSelector((state) => state.posts);
+  const classes = useStyles();
+
+  return (
+    !posts.length ? <CircularProgress /> : (
+      <Grid className={classes.container} container alignItems="stretch" spacing={3}>
+        {posts.map((post) => (
+          <Grid key={post._id} item xs={12} sm={6} md={6}>
+            <Post post={post} setCurrentId={setCurrentId} />
+          </Grid>
+        ))}
+      </Grid>
     )
-}
+  );
+};
 
-export default Posts
+export default Posts;
