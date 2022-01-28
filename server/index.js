8bce23d6a7a3223fcfcc6e7692bd7f4aed95351a
tiyajfrancis <tjf31102002@gmail.com>
2022-01-28 20:32:52 +0400

    added tiya's and ratiq's code to main.
    waiting for completion of group chat code to fully incorporated whiteboard code into Faizan's existing code.

--- server/index.js
+++ server/index.js
@@ -1,4 +1,3 @@
-
 import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
@@ -8,6 +7,17 @@ import postRoutes from './routes/posts.js';
 import userRouter from "./routes/user.js";
 
 const app = express();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+
+io.on('connection', (socket) => {
+  console.log('User Online');
+
+  socket.on('canvas-data', (data) => {
+      socket.broadcast.emit('canvas-data', data);
+
+  })
+})
 
 app.use(express.json({ limit: '30mb', extended: true }))
 app.use(express.urlencoded({ limit: '30mb', extended: true }))
@@ -17,7 +27,7 @@ app.use('/posts', postRoutes);
 app.use("/user", userRouter);
 
 const CONNECTION_URL = 'mongodb+srv://fhp1:hacker2.O@cluster0.cyyxs.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
-const PORT = process.env.PORT|| 5000;
+const PORT = process.env.YOUR_PORT || process.env.PORT|| 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
