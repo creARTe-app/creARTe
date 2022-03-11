6159f2917373e60638a97b97c56640f8e247c148
tiyajfrancis <67215949+tiyajfrancis@users.noreply.github.com>
2022-03-11 21:49:35 +0400

    added brush size slider

--- client/src/components/Container/Container.jsx
+++ client/src/components/Container/Container.jsx
@@ -1,4 +1,3 @@
-// @Author Ratiq T.
 import React from "react";
 import Board from "../Board/Board";
 import './style.css';
@@ -6,18 +5,52 @@ import './style.css';
 class container extends React.Component{
     constructor(props){
         super(props);
+
+        this.state = {
+            color: "#00000",
+            size: "50"
+        }
     }
+
+    newColor(params){
+        this.setState({
+            color: params.target.value
+        })   
+    }
+
+    brushSize(params){
+        this.setState({
+            size: params.target.value
+        })   
+    }
+
     render(){
         return(
             <div className="container">
                 <div className="color-container">
-                </div>    
+                    <div>
+                        Color: &nbsp;
+                    </div>
+                    <input type="color" value={this.state.color} onChange={this.newColor.bind(this)}/>
+                </div>
+
                 <div className="button-container">
-                    <button onclick="">Save</button>
-                    <button onclick="">Import</button>
+                    <button className="file-buttons" id="save"><i class="fa fa-save"></i></button>
+                    <label className="file-buttons" for="fileInput"> 
+                        <i class="fa fa-upload"></i> 
+                    </label>
+                    <input id="fileInput" type="file"/>
                 </div> 
+
+                <div className="brushsize-container">
+                    <div>
+                        Size: &nbsp;
+                    </div>
+                    <input id="slider" type="range" min="5" max="100" value={this.state.size} onChange={this.brushSize.bind(this)}></input>
+                </div>
+
                 <div class="board-container">
-                    <Board> </Board>
+                    <Board color={this.state.color} size={this.state.size}></Board>
                 </div>
             </div>
         )
