d38c1595e687fa6d68351d9a33578120f3800219
Faizzann <71325258+Faizzann@users.noreply.github.com>
2022-03-11 22:52:22 +0400

    updaated styles and home page functionality

---
+++ client/src/components/Auth/Auth.js
@@ -0,0 +1,93 @@
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { Avatar, Button, Paper, Grid, Typography, Container } from '@material-ui/core';
+import { useHistory } from 'react-router-dom';
+import Cookies from 'universal-cookie';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import axios from 'axios';
+
+import { signin, signup } from '../../actions/auth';
+import useStyles from './styles';
+import Input from './Input';
+
+const cookies = new Cookies();
+const initialState = { firstName: '', lastName: '', email: '', password: '', confirmPassword: '' };
+
+const SignUp = () => {
+  const [form, setForm] = useState(initialState);
+  const [isSignup, setIsSignup] = useState(false);
+  const dispatch = useDispatch();
+  const history = useHistory();
+  const classes = useStyles();
+
+  const [showPassword, setShowPassword] = useState(false);
+  const handleShowPassword = () => setShowPassword(!showPassword);
+
+  const switchMode = () => {
+    setForm(initialState);
+    setIsSignup((prevIsSignup) => !prevIsSignup);
+    setShowPassword(false);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+        // const { email, password } = form;
+        // const URL = 'http://localhost:5000/auth';
+        
+        // const { data: { token, userId, confirmPassword, firstName, lastName } } = await axios.post(`${URL}/${isSignup ? 'signup' : 'login'}`, {
+        //     email, password, firstName, lastName: form.fullName, 
+        // });    
+        
+        // cookies.set('token', token);
+        // cookies.set('email', email);
+        // cookies.set('firstName', firstName);
+        // cookies.set('lastName', lastName);
+        // cookies.set('userId', userId);
+
+    if (isSignup) {
+      dispatch(signup(form, history));
+      // cookies.set('confirmPassword', confirmPassword);
+    } else {
+      dispatch(signin(form, history));
+    }
+  };
+
+  const handleChange = (e) => setForm({ ...form, [e.target.name]: e.target.value });
+
+  return (
+    <Container component="main" maxWidth="xs">
+      <Paper className={classes.paper} elevation={6}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">{ isSignup ? 'Sign up' : 'Sign in' }</Typography>
+        <form className={classes.form} onSubmit={handleSubmit}>
+          <Grid container spacing={2}>
+            { isSignup && (
+            <>
+              <Input name="firstName" label="First Name" handleChange={handleChange} autoFocus half />
+              <Input name="lastName" label="Last Name" handleChange={handleChange} half />
+            </>
+            )}
+            <Input name="email" label="Email Address" handleChange={handleChange} type="email" />
+            <Input name="password" label="Password" handleChange={handleChange} type={showPassword ? 'text' : 'password'} handleShowPassword={handleShowPassword} />
+            { isSignup && <Input name="confirmPassword" label="Repeat Password" handleChange={handleChange} type="password" /> }
+          </Grid>
+          <Button type="submit" fullWidth variant="contained"  className={classes.submit}>
+            { isSignup ? 'Sign Up' : 'Sign In' }
+          </Button>
+          <Grid container justify="flex-end">
+            <Grid item >
+              <Button onClick={switchMode} >
+                { isSignup ? 'Already have an account, would you like to sign in instead? ' : "Don't have an account, would you like to sign up!" }
+              </Button>
+            </Grid>
+          </Grid>
+        </form>
+      </Paper>
+    </Container>
+  );
+};
+
+export default SignUp;
---
+++ client/src/components/Navbar/Dropdown.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+
+function Dropdown({ title, items, multiSelect = false }) {
+  const [open, setOpen] = useState(false);
+  const [selection, setSelection] = useState([]);
+  
+
+   return (
+    <div className="dd-wrapper">
+      <div
+        tabIndex={0}
+        className="dd-header"
+        role="button"
+        onKeyPress={() => toggle(!open)}
+        onClick={() => toggle(!open)}
+      >
+        <div className="dd-header__title">
+          <p className="dd-header__title--bold">{title}</p>
+        </div>
+        <div className="dd-header__action">
+          <p>{open ? 'Close' : 'Open'}</p>
+        </div>
+      </div>
+      {open && (
+        <ul className="dd-list">
+          {items.map(item => (
+            <li className="dd-list-item" key={item.id}>
+              <button type="button" onClick={() => handleOnClick(item)}>
+                <span>{item.value}</span>
+                <span>{isItemInSelection(item) && 'Selected'}</span>
+              </button>
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+   );
+}
