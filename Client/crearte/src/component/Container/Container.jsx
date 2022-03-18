9702febd21282aa868e5513bfd0617fbf2d56c88
tiyajfrancis <tjf31102002@gmail.com>
2022-03-18 12:37:30 +0400

    menu commit

--- Client/crearte/src/component/Container/Container.jsx
+++ Client/crearte/src/component/Container/Container.jsx
@@ -27,31 +27,48 @@ class container extends React.Component{
     render(){
         return(
             <div className="container">
-                <div className="color-container">
-                    <div>
-                        Color: &nbsp;
-                    </div>
-                    <input type="color" value={this.state.color} onChange={this.newColor.bind(this)}/>
-                </div>    
-                <div className="button-container">
-                    {/* <button id="save">Save</button> */}
-                    <button className="file-buttons" id="save"><i class="fa fa-save"></i></button>
-                    <label className="file-buttons" for="fileInput"> 
-                        <i class="fa fa-upload"></i> 
-                    </label>
-                     <input id="fileInput" type="file"/>{/*onClick="setImage()"/> */}
-                    {/* <img id="preview"></img> */}
-                     {/* <button className="file-buttons" id="import">
-                        <i class="fa fa-upload" ></i> 
-                    </button> */}
-                </div> 
-                <div className="brushsize-container">
-                    <div>
-                        Size: &nbsp;
-                    </div>
-                    <input id="slider" type="range" min="5" max="100" value={this.state.size} onChange={this.brushSize.bind(this)}></input>
-                </div>
+                <nav class="main-menu">
+                
+                    <div class="settings"></div>
+                    <div class="scrollbar" id="style-1"></div>
+                    <ul>
+                    
+                            <li>
+                                <a>
+                                    <i class="fa fa-paint-brush fa-lg"></i> 
+                                    <span className="nav-text"><input type="color" value={this.state.color} onChange={this.newColor.bind(this)}></input></span>
+                                </a>
+                                
+                                
+                            
+                            </li>
 
+                            <li>
+                                <a>
+                                    <i class="fa fa-paint-brush fa-lg"></i> 
+                                    <span class="nav-text"><input id="slider" type="range" min="5" max="100" value={this.state.size} onChange={this.brushSize.bind(this)}></input></span>
+                                </a>
+                            </li>
+
+                            
+                            <li>
+                                
+                                <button className="file-buttons" id="save">
+                                    <i class="fa fa-save fa-lg"></i>
+                                </button>
+
+                            </li>
+
+                            <li>
+                                <label className="file-buttons" for="fileInput"> 
+                                    <i class="fa fa-upload fa-lg"></i> 
+                                </label>
+                                <input id="fileInput" type="file"/>
+                            </li>
+                            
+                    </ul>
+                </nav>
+                
                 <div class="board-container">
                     <Board color={this.state.color} size={this.state.size}></Board>
                 </div>
