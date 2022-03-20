f1e4a7b783554b441fa2edf39ab97f3c7ccb05be
Faizzann <71325258+Faizzann@users.noreply.github.com>
2022-03-20 13:25:05 +0400

    Add files via upload
    admin implementation started

---
+++ client/src/components/Dropdown/Dropdown.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+import { Menu, MenuItem, Button } from '@material-ui/core';
+import PopupState, { bindTrigger, bindMenu } from 'material-ui-popup-state';
+import { useDispatch } from 'react-redux';
+import { Link, useHistory } from 'react-router-dom';
+import useStyles from './styles';
+import * as actionType from '../../constants/actionTypes';
+
+export default function MenuPopupState() {
+  const [user, setUser] = useState(JSON.parse(localStorage.getItem('profile')));
+  const history = useHistory();
+  const dispatch = useDispatch();
+
+  const logout = () => {
+    dispatch({ type: actionType.LOGOUT });
+
+    history.push('/auth');
+
+    setUser(null);
+  };
+  const classes = useStyles();
+
+  return (
+    <PopupState variant="popover" popupId="demo-popup-menu">
+      {(popupState) => (
+        <React.Fragment>
+          <Button className={classes.dashboard} variant="contained" {...bindTrigger(popupState)}>
+            Dashboard
+          </Button>
+          <Menu {...bindMenu(popupState)} >
+            <MenuItem className={classes.options} onClick={popupState.close} component={Link} to="/">Home</MenuItem>
+            <MenuItem className={classes.options} onClick={popupState.close} component={Link} to="/profile">Profile</MenuItem>
+            <MenuItem className={classes.options} onClick={popupState.close} component={Link} to="/chats">Chat</MenuItem>
+            <MenuItem className={classes.logout} onClick={logout}>Logout</MenuItem>
+          </Menu>
+        </React.Fragment>
+      )}
+    </PopupState>
+  );
+}
---
+++ server/models/postMessage.js
@@ -0,0 +1,24 @@
+import mongoose from 'mongoose';
+
+const postSchema = mongoose.Schema({
+    title: String,
+    message: String,
+    name: String,
+    creator: String,
+    tags: [String],
+    selectedFile: String,
+    likes: { type: [String], default: [] },
+    comments: [{
+        comment: {
+            type: String       
+         }
+        }],
+    createdAt: {
+        type: Date,
+        default: new Date(),
+    },
+})
+
+var PostMessage = mongoose.model('PostMessage', postSchema);
+
+export default PostMessage;
