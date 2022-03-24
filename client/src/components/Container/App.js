7193fc40a10200f45cd8ea904886c9d482b28c90
Faizzann <71325258+Faizzann@users.noreply.github.com>
2022-03-25 00:09:51 +0400

    Add files via upload
    faizan final

---
+++ client/src/components/Container/App.js
@@ -0,0 +1,52 @@
+import "./App.css";
+import io from "socket.io-client";
+import React, { useState } from "react";
+import Container from "../Container/Container";
+
+
+const socket = io.connect("http://localhost:3001");
+
+function App() {
+  const [username, setUsername] = useState("");
+  const [room, setRoom] = useState("");
+  const [showChat, setShowChat] = useState(false);
+
+  const joinRoom = () => {
+    if (username !== "" && room !== "") {
+      socket.emit("join_room", room);
+      setShowChat(true);
+      document.getElementById("room-form").classList.remove("App");
+    }
+  };
+
+  return (
+    <>
+    <div class="App" id="room-form">
+      {!showChat ? (
+        <div className="joinChatContainer">
+          <h3>Join A Room</h3>
+          <input
+            type="text"
+            placeholder="Name..."
+            onChange={(event) => {
+              setUsername(event.target.value);
+            }}
+          />
+          <input
+            type="text"
+            placeholder="Chatroom ID..."
+            onChange={(event) => {
+              setRoom(event.target.value);
+            }}
+          />
+          <button onClick={joinRoom}>Join</button>     
+        </div>
+      ) : (
+        <Container socket={socket} username={username} room={room} />
+      )}
+    </div>
+    </>
+  );
+}
+
+export default App;
