8950b6374ecad1a3026a411846fd638867fb4798
Faizzann <71325258+Faizzann@users.noreply.github.com>
2022-03-27 20:38:35 +0400

    Delete client directory

--- client/src/reducers/index.js
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from 'redux';
-
-import posts from './posts';
-import auth from './auth';
-import savedPosts from './savedPosts'
-
-export const reducers = combineReducers({ posts, auth, savedPosts });
