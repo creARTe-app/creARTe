7803d1c7e9461669b29eddb8c877dff8fe058aab
Mohamed Ratiq <mrt2000@hw.ac.uk>
2022-03-29 23:59:18 +0400

    fixed empty form bug

--- creARTe-main/client/src/components/Form/Form.js
+++ creARTe-main/client/src/components/Form/Form.js
@@ -29,12 +29,20 @@ const Form = ({ currentId, setCurrentId }) => {
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    if (currentId === 0) {
-      dispatch(createPost({ ...postData, name: user?.result?.name }, history));
-      clear();
-    } else {
-      dispatch(updatePost(currentId, { ...postData, name: user?.result?.name }));
-      clear();
+    if(postData.title == '' ){
+      alert("Please enter a title")
+    }
+    else if(postData.selectedFile == ''){
+      alert("Please choose a picture to upload")
+    }
+    else{
+      if (currentId === 0) {
+        dispatch(createPost({ ...postData, name: user?.result?.name }, history));
+        clear();
+      } else {
+        dispatch(updatePost(currentId, { ...postData, name: user?.result?.name }));
+        clear();
+      }
     }
   };
 
@@ -48,32 +56,32 @@ const Form = ({ currentId, setCurrentId }) => {
     );
   }
 
-//   const handleAddChip = (tag) => {
-//     setPostData({ ...postData, tags: [...postData.tags, tag] });
-//   };
+  const handleAddChip = (tag) => {
+    setPostData({ ...postData, tags: [...postData.tags, tag] });
+  };
 
-//   const handleDeleteChip = (chipToDelete) => {
-//     setPostData({ ...postData, tags: postData.tags.filter((tag) => tag !== chipToDelete) });
-//   };
+  const handleDeleteChip = (chipToDelete) => {
+    setPostData({ ...postData, tags: postData.tags.filter((tag) => tag !== chipToDelete) });
+  };
 
   return (
     <Paper className={classes.paper} elevation={6}>
       <form autoComplete="off" noValidate className={`${classes.root} ${classes.form}`} onSubmit={handleSubmit}>
         <Typography variant="h6">{currentId ? `Editing "${post?.title}"` : 'Uploading a post'}</Typography>
-        <TextField name="title" variant="outlined" label="Title" fullWidth value={postData.title} onChange={(e) => setPostData({ ...postData, title: e.target.value })} />
+        <TextField name="title" required id='nameId' variant="outlined" label="Title" fullWidth value={postData.title} onChange={(e) => setPostData({ ...postData, title: e.target.value })} />
         <TextField name="message" variant="outlined" label="Message" fullWidth multiline rows={4} value={postData.message} onChange={(e) => setPostData({ ...postData, message: e.target.value })} />
-//         <div style={{ padding: '5px 0', width: '94%' }}>
-//           <ChipInput
-//             name="tags"
-//             variant="outlined"
-//             label="Tags"
-//             fullWidth
-//             value={postData.tags}
-//             onAdd={(chip) => handleAddChip(chip)}
-//             onDelete={(chip) => handleDeleteChip(chip)}
-//           />
-//         </div>
-        <div className={classes.fileInput}><FileBase type="file" multiple={false} onDone={({ base64 }) => setPostData({ ...postData, selectedFile: base64 })} /></div>
+        <div style={{ padding: '5px 0', width: '94%' }}>
+          <ChipInput
+            name="tags"
+            variant="outlined"
+            label="Tags"
+            fullWidth
+            value={postData.tags}
+            onAdd={(chip) => handleAddChip(chip)}
+            onDelete={(chip) => handleDeleteChip(chip)}
+          />
+        </div>
+        <div className={classes.fileInput}><FileBase type="file" required multiple={false} onDone={({ base64 }) => setPostData({ ...postData, selectedFile: base64 })} /></div>
         <Button className={classes.buttonSubmit} variant="contained" color="primary" size="large" type="submit" fullWidth>Submit</Button>
         <Button variant="contained" size="small" className={classes.buttonClear} onClick={clear} fullWidth>Clear</Button>
       </form>
